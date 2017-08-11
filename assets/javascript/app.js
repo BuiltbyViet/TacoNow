c5bf91b6ca39c96dd7572fb46029d6ba83bda330
powi442 <k08ip01@gmail.com>
2017-08-11 12:02:45 -0700

    geo
    geolocation draft 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,81 @@
+var map;
+var infowindow;
+
 function initMap() {
-        var ucIrvine = {lat: 33.6405 , lng: -117.8443};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: ucIrvine
-        });
-        var marker = new google.maps.Marker({
-          position: ucIrvine,
-          map: map
-        });
-      }
+  var ucIrvine = {lat: 33.6405, lng: -117.8443};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: ucIrvine,
+    zoom: 12
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: ucIrvine,
+    radius: 1000,
+    type: ['restaurant']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+// Note: This example requires that you consent to location sharing when
+     // prompted by your browser. If you see the error "The Geolocation service
+     // failed.", it means you probably did not give permission for the browser to
+     // locate you.
+     var map, infoWindow;
+     function initMap() {
+       map = new google.maps.Map(document.getElementById('map'), {
+         center: {lat: -34.397, lng: 150.644},
+         zoom: 12
+       });
+       infoWindow = new google.maps.InfoWindow;
+
+       // Try HTML5 geolocation.
+       if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var pos = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+           };
+
+           infoWindow.setPosition(pos);
+           infoWindow.setContent('Location found.');
+           infoWindow.open(map);
+           map.setCenter(pos);
+         }, function() {
+           handleLocationError(true, infoWindow, map.getCenter());
+         });
+       } else {
+         // Browser doesn't support Geolocation
+         handleLocationError(false, infoWindow, map.getCenter());
+       }
+     }
+
+     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+       infoWindow.setPosition(pos);
+       infoWindow.setContent(browserHasGeolocation ?
+                             'Error: The Geolocation service failed.' :
+                             'Error: Your browser doesn\'t support geolocation.');
+       infoWindow.open(map);
+     }
