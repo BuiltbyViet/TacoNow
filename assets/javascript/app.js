9b08797a1fbdc327a05fbef6742d6ee50173e87c
powi442 <k08ip01@gmail.com>
2017-08-14 10:45:28 -0700

    added viet's js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,21 @@
-var map;
-var infowindow;
-var searchBox;
-
-function initMap() {
-  var ucIrvine = {lat: 33.6405, lng: -117.8443};
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: ucIrvine,
-    zoom: 12
-  });
-
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: ucIrvine,
-    radius: 1000,
-    type: 'restaurant',
-    keyword: 'taco'
-  }, callback);
-}
-
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
 // ___________________New Search Feature: Andrew________________________
 $(document).ready(function() {
 var input = document.getElementById('pac-input');
-input.value = "tacos";
+input.value = "mexican";
 setTimeout(function() {
        google.maps.event.trigger(input, 'focus')
         google.maps.event.trigger(input, 'keydown', {
             keyCode: 13
         });
 
-}, 500);
+}, 200);
 
 })
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.6405, lng: -117.8443},
-    zoom: 13,
-    mapTypeId: 'roadmap'
+    zoom: 13
+
   });
 
   // Create the search box and link it to the UI element.
@@ -122,3 +79,39 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
+
+//=== viet's code
+
+$("#submit-location").on("click", function(event){
+event.preventDefault();
+initMap();
+});
+
+
+
+
+function initMap() {
+  var zipCode = $("#input-location").val();
+var queryURL =  "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:"+zipCode+"&key=AIzaSyDC5VjVV78MqkJggO81SnzhUxDyF1HUfGI";
+
+     console.log(zipCode) ;
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+.done(function(response) {
+
+console.log(response);
+
+
+        var ucIrvine = {lat: response.results[0].geometry.location.lat, lng: response.results[0].geometry.location.lng};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: ucIrvine
+        });
+        var marker = new google.maps.Marker({
+          position: ucIrvine,
+          map: map
+        });
+      });
+};
