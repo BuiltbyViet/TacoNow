a6bb19ee08e582f7fbd33dea2d9a17e52219749d
Phuoc-Viet Nguyen <sovietnguyen2291@gmail.com>
2017-08-14 12:03:50 -0700

    zipcode and taco icon works once

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,21 @@
+// ___________________New Search Feature: Andrew________________________
+$(document).ready(function() {
+var input = document.getElementById('pac-input');
+input.value = "mexican";
+setTimeout(function() {
+       google.maps.event.trigger(input, 'focus')
+        google.maps.event.trigger(input, 'keydown', {
+            keyCode: 13
+        });
 
+}, 200);
 
-$("#submit-location").on("click", function(event){
-event.preventDefault();
-initMap();
-});
-
-
-
-
-function initMap() {
-  var zipCode = $("#input-location").val();
+})
+function initAutocomplete() {
+ var zipCode = $("#input-location").val();
 var queryURL =  "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:"+zipCode+"&key=AIzaSyDC5VjVV78MqkJggO81SnzhUxDyF1HUfGI";
 
-     console.log(zipCode) ;  
+     console.log(zipCode) ;
 $.ajax({
   url: queryURL,
   method: "GET"
@@ -27,21 +30,84 @@ console.log(response);
           zoom: 12,
           center: ucIrvine
         });
-        var marker = new google.maps.Marker({
+        var newMark = new google.maps.Marker({
           position: ucIrvine,
           map: map
         });
       });
-};
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  console.log(input)
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    console.log(markers)
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: "assets/images/tacomap.png",
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+//=== viet's code
+
+$("#submit-location").on("click", function(event){
+event.preventDefault();
+initAutocomplete();
+});
 
 
 
 
-
-
-
-
-  
 
 
 
