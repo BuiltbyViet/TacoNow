681d4349bc765b49137f172f67bea41ae4ab5c82
Phuoc-Viet Nguyen <sovietnguyen2291@gmail.com>
2017-08-16 09:33:58 -0700

    fixed taco markers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
+
+//when clicking the locoation search button, initiate function and map appears
 $("#submit-location").on("click", function(event) {
     event.preventDefault();
     initAutocomplete();
     $("#mapDiv").animate({opacity:"1"});
 });
 
+//retrieve info from geocode api and use lat and lng for location
 function initAutocomplete() {
     var location = $("#input-location").val();
     var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyDC5VjVV78MqkJggO81SnzhUxDyF1HUfGI";
@@ -23,16 +26,16 @@ function initAutocomplete() {
                 center: mapLocation
             });
 
-
+//find any 'mexican' related places on map within 500 meters
             var infowindow;
             infowindow = new google.maps.InfoWindow();
             var service = new google.maps.places.PlacesService(map);
             service.textSearch({
                 location: mapLocation,
                 radius: 500,
-                query: 'mexican'
+                query: 'mexican food',
             }, callback);
-
+//criteria for creating marker
             function callback(results, status) {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
                     for (var i = 0; i < results.length; i++) {
@@ -42,7 +45,7 @@ function initAutocomplete() {
             }
 
             // -------------Taco Icon----------------------------
-
+//change marker icon to Taco
             var icon = {
                 url: "assets/images/tacomap.png",
                 size: new google.maps.Size(71, 71),
@@ -52,6 +55,7 @@ function initAutocomplete() {
             };
 
             // ---------------------------------------------------
+            //create markers based on search
             function createMarker(place) {
                 var placeLoc = place.geometry.location;
                 var marker = new google.maps.Marker({
@@ -59,13 +63,14 @@ function initAutocomplete() {
                     icon: icon,
                     position: place.geometry.location
                 });
+                //display info upon click on marker
                 google.maps.event.addListener(marker, 'click', function() {
                     infowindow.setContent(place.name + "<br>" + "Address: " + place.formatted_address + "<br>" + "Rating: " + place.rating + "<br>" + "Open: " + place.opening_hours.open_now);
                     infowindow.open(map, this);
                     console.log(place);
 
                     // -------Display Results-----------------------
-
+                    // create div for each selection clicked
                     var resultsDiv = $('<div>');
                     resultsDiv.attr('class', 'panel panel-primary clickOptions')
                     var p = $("<p>").html("<b>"+place.name+"</b>" + "<br>" + "Address: " + place.formatted_address + "<br>" + "Rating: " + place.rating+ "/ 5");
